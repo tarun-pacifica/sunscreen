7480ad8e3ebb6b266a0cb68c93e8d6168d6d40d7
Tarun Mookhey <tarun.mookhey@reinteractive.net>
2017-07-14 18:06:39 +1000

    redirects added

--- app/controllers/sunscreen_controller.rb
+++ app/controllers/sunscreen_controller.rb
@@ -4,7 +4,19 @@ class SunscreenController < ApplicationController
   def show
     @city = City.find params[:city].to_i
     @uv_index = Radiation.uv_index @city.name
-     render partial: "naked" if @uv_index < 0.2
-     render partial: "burnt" if @uv_index >= 0.2
+    redirect_to naked_path(name: @city.name,index: @uv_index) if @uv_index == 0
+    redirect_to burnt_path(name: @city.name,index: @uv_index) if @uv_index != 0
+  end
+
+  def naked
+  	@city = params[:name]
+  	@uv_index = params[:index]
+  	render "naked"
+  end
+
+  def burnt
+  	@city = params[:name]
+  	@uv_index = params[:index]
+  	render "burnt"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :sunscreen, only:[:index,:show]
   resources :about, only: [:index]
   root "sunscreen#index"
+  get '/naked' => "sunscreen#naked"
+  get '/burnt' => "sunscreen#burnt"
 end
